09c951595d30b92e01a11b3621baf08bb0114f97
Lone Ranger <dreamingforward@gmail.com>
2018-08-02 15:59:23 -0500

    Create kernel.c

---
+++ kernel.c
@@ -0,0 +1,17 @@
+#Purpose: starts the core operating system (previous to any user commands)
+#Donewhen: user requests
+#Exit_status:
+#"all apps shut down normally"
+#"all hardware shutdown normally"
+#"rebooting"
+
+//first function comment*/
+exit_status kernel(post_result):{
+//all core hardware ready and ready to respond to user requests
+    var=0;
+    return b;
+};
+/*comment for the following function*/
+INT_MAX function2():{
+    this=that
+};
